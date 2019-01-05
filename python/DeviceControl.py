b7600aab64b054ca3f822c3b8dc618ba8b2f1e66
Nathan Robinson <nrobinson2000@me.com>
2019-01-05 15:41:35 -0500

    DeviceControl implemented at 15:41 of 2019-01-05

--- python/DeviceControl.py
+++ python/DeviceControl.py
@@ -1,55 +1,104 @@
 #!/usr/bin/env python3
 
 import serial
+import serial.tools.list_ports
 import subprocess
 import time
+from platform import system
 
 class DeviceControl:
 
     def __init__(self, platform, portName):
-        self.platform = platform
 
-        if platform is "xenon":
-            self.usbID = "2b04:d00e"
-            self.dfuAddress = "0x000d4000"
-        elif platform is "argon":
-            self.usbID = "2b04:d00c"
-            self.dfuAddress = "0x000d4000"
-        elif platform is "boron":
-            self.usbID = "2b04:d00d"
-            self.dfuAddress = "0x000d4000"
-        elif platform is "photon":
-            self.usbID = "2b04:d006"
-            self.dfuAddress = "0x080A0000"
-        elif platform is "P1":
-            self.usbID = "2b04:d008"
-            self.dfuAddress = "0x080A0000"
-        elif platform is "electron":
-            self.usbID = "2b04:d00a"
-            self.dfuAddress = "0x08080000"
-        elif platform is "core":
-            self.usbID = "1d50:607f"
-            self.dfuAddress = "0x08005000"
-        elif platform is "duo":
-            self.usbID = "2b04:d058"
-            self.dfuAddress = "0x80C0000"
-        elif platform is "none":
+        # Try dfu-util -l method first
+        try:
+            self.usbID = subprocess.check_output(["dfu-util", "--list"]).splitlines()[-1].split()[2][1:-1].decode('UTF-8').lower()
+        except IndexError:
             pass
         else:
-            print("Invalid platform!")
+            if self.usbID == "2b04:d00e":
+                self.platform = 'xenon'
+                self.dfuAddress = "0x000d4000"
+            elif self.usbID == "2b04:d00c":
+                self.platform = 'argon'
+                self.dfuAddress = "0x000d4000"
+            elif self.usbID == "2b04:d00d":
+                self.platform = 'boron'
+                self.dfuAddress = "0x000d4000"
+            elif self.usbID == "2b04:d006":
+                self.platform = 'photon'
+                self.dfuAddress = "0x080A0000"
+            elif self.usbID == "2b04:d008":
+                self.platform = 'P1'
+                self.dfuAddress = "0x080A0000"
+            elif self.usbID == "2b04:d00a":
+                self.platform = 'electron'
+                self.dfuAddress = "0x08080000"
+            elif self.usbID =="1d50:607f":
+                self.platform = 'core'
+                self.dfuAddress = "0x08005000"
+            elif self.usbID == "2b04:d058":
+                self.platform = 'duo'
+                self.dfuAddress = "0x80C0000"
+            else:
+                raise Exception("Invalid usbID!")
 
         if portName == "auto":
-            ports = serial.tools.list_ports.grep("/dev/cu.usbmodem")
+            if system() == "Darwin":
+                ports = serial.tools.list_ports.grep("/dev/cu.usbmodem")
             connectedPorts = []
             for port in ports:
-                connectedPorts.append(port.device)
+                connectedPorts.append(port)
             if len(connectedPorts) < 1:
-                print("Could not find a port!")
-                quit()
-            self.portName = connectedPorts[0]
+                raise Exception("Could not find a port!")
+            devicePort = connectedPorts[0]
+            self.portName = devicePort.device
+
+            if platform == 'auto':
+                self.platform = devicePort.product.split()[0].lower()
         else:
             self.portName = portName
 
+            if platform == 'auto':
+                ports = serial.tools.list_ports.grep(self.portName)
+                connectedPorts = []
+                for port in ports:
+                    connectedPorts.append(port)
+                if len(connectedPorts) < 1:
+                    raise Exception("Could not determine platform!")
+                devicePort = connectedPorts[0]
+                self.platform = devicePort.product.split()[0].lower()
+            else:
+                self.platform = platform
+
+        if self.platform == 'xenon':
+            self.usbID = "2b04:d00e"
+            self.dfuAddress = "0x000d4000"
+        elif self.platform == 'argon':
+            self.usbID = "2b04:d00c"
+            self.dfuAddress = "0x000d4000"
+        elif self.platform == 'boron':
+            self.usbID = "2b04:d00d"
+            self.dfuAddress = "0x000d4000"
+        elif self.platform == 'photon':
+            self.usbID = "2b04:d006"
+            self.dfuAddress = "0x080A0000"
+        elif self.platform == 'P1':
+            self.usbID = "2b04:d008"
+            self.dfuAddress = "0x080A0000"
+        elif self.platform == 'electron':
+            self.usbID = "2b04:d00a"
+            self.dfuAddress = "0x08080000"
+        elif self.platform == 'core':
+            self.usbID = "1d50:607f"
+            self.dfuAddress = "0x08005000"
+        elif self.platform == 'duo':
+            self.usbID = "2b04:d058"
+            self.dfuAddress = "0x80C0000"
+        elif self.platform == "none":
+            pass
+        else:
+            print("Invalid platform!")
 
         self.neutralBaudRate = 9600
         self.dfuBaudRate = 14400
@@ -61,19 +110,22 @@ class DeviceControl:
             ser.close()
 
         except serial.serialutil.SerialException:
-            print("Invalid Serial Port!")
-            quit()
-            
-        else:
-            try:
-                ser = serial.Serial(self.portName, self.neutralBaudRate)
-                ser.close()
+            raise Exception("Could not open that serial port!")
 
-            except serial.serialutil.SerialException:
-                pass
+        else:
+            if system() == "Linux":
+                try:
+                    ser = serial.Serial(self.portName, self.neutralBaudRate)
+                    ser.close()
+
+                except serial.serialutil.SerialException:
+                    raise Exception("Invalid Serial Port!")
 
     def closeDFU(self):
-        subprocess.run(["dfu-util", "-d", self.usbID, "-a", "0", "-i", "0", "-s", self.dfuAddress + ":leave", "-D", "/dev/null"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        try:
+            subprocess.run(["dfu-util", "-d", self.usbID, "-a", "0", "-i", "0", "-s", self.dfuAddress + ":leave", "-D", "/dev/null"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except AttributeError:
+            raise Exception("Could not find a device in DFU!")
 
     def openListening(self):
         try:
@@ -81,8 +133,7 @@ class DeviceControl:
             ser.close()
 
         except serial.serialutil.SerialException:
-            print("Invalid Serial Port!")
-            quit()
+            raise Exception("Could not open that serial port!")
 
         else:
             try:
@@ -93,6 +144,32 @@ class DeviceControl:
                 pass
 
     def closeListening(self):
-        self.openDFU()
-        time.sleep(1)
-        self.closeDFU()
+        try:
+            self.openDFU()
+            time.sleep(1)
+            self.closeDFU()
+        except Exception as error:
+            print(repr(error))
+
+    def openSafe(self):
+        try:
+            self.openListening()
+        except Exception as error:
+            print(repr(error))
+        else:
+            ser = serial.Serial(self.portName, self.neutralBaudRate)
+            ser.write(b'L')
+            time.sleep(10)
+            ser = serial.Serial(self.portName, self.neutralBaudRate)
+            ser.write(b'x')
+
+        # try:
+        #     ser = serial.Serial(self.portName, self.neutralBaudRate)
+        #     ser.write(b'L')
+        #     time.sleep(4)
+        #     ser = serial.Serial(self.portName, self.neutralBaudRate)
+        #     ser.write(b'x')
+        #     ser.close()
+        #
+        # except serial.serialutil.SerialException:
+        #     pass
--- python/test.py
+++ python/test.py
@@ -1,37 +1,81 @@
 #!/usr/bin/env python3
 
+# Load completion script
+# complete -F _po-util ./test.py
+
 from DeviceControl import DeviceControl as device
-
-
-import serial.tools.list_ports
 import sys
 
 print(sys.argv)
 
-photon = device("photon", "auto")
-
 if sys.argv[1] == 'serial':
     if sys.argv[2] == 'open':
         try:
-            portName = sys.argv[3]
+            portName = sys.argv[4]
         except IndexError:
-            myDevice = device("none", "auto")
-            myDevice.openListening()
+            try:
+                myDevice = device("auto", "auto")
+                myDevice.openListening()
+            except Exception as error:
+                print(repr(error))
         else:
-            myDevice = device("none", portName)
-            myDevice.openListening()
+            try:
+                myDevice = device("none", portName)
+                myDevice.openListening()
+            except Exception as error:
+                print(repr(error))
     if sys.argv[2] == 'close':
         try:
-            portName = sys.argv[3]
+            portName = sys.argv[4]
         except IndexError:
-            myDevice = device("none", "auto")
-            myDevice.closeListening()
+            try:
+                myDevice = device("auto", "auto")
+                myDevice.closeListening()
+            except Exception as error:
+                print(repr(error))
         else:
-            myDevice = device("none", portName)
-            myDevice.closeListening()
+            try:
+                myDevice = device("auto", portName)
+                myDevice.closeListening()
+            except Exception as error:
+                print(repr(error))
 
 if sys.argv[1] == 'dfu':
     if sys.argv[2] == 'open':
-        photon.openDFU()
+        try:
+            portName = sys.argv[4]
+        except IndexError:
+            try:
+                myDevice = device("auto", "auto")
+                myDevice.openDFU()
+            except Exception as error:
+                print(repr(error))
+        else:
+            try:
+                myDevice = device("none", portName)
+                myDevice.openDFU()
+            except Exception as error:
+                print(repr(error))
     if sys.argv[2] == 'close':
-        photon.closeDFU()
+        try:
+            myDevice = device("none", "none")
+            myDevice.closeDFU()
+        except Exception as error:
+            print(repr(error))
+
+if sys.argv[1] == 'safe':
+    if sys.argv[2] == 'open':
+        try:
+            portName = sys.argv[4]
+        except IndexError:
+            try:
+                myDevice = device("auto", "auto")
+                myDevice.openSafe()
+            except Exception as error:
+                print(repr(error))
+        else:
+            try:
+                myDevice = device("none", portName)
+                myDevice.openSafe()
+            except Exception as error:
+                print(repr(error))
